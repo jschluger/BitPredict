0f59954e220f73978c5ab08fb8f6ad93c55e581c
Jack Schluger <jschluger@stuy.edu>
2017-11-29 16:47:43 -0500

    adding new xcode project

---
+++ BitPredict/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitPredict
+//
+//  Created by eoin on 11/29/17.
+//  Copyright © 2017 panna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
